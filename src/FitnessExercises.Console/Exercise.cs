7522903c2b69facd2f4107930f95a520b941ca1c
Simon Kofodm <skfd@novonordisk.com>
2025-08-06 10:14:08 +0200

    Extract to json

---
+++ src/FitnessExercises.Console/Exercise.cs
@@ -0,0 +1,14 @@
+namespace FitnessExercises.Console;
+
+public class Exercise
+{
+    public string Type { get; set; } = string.Empty;
+    public string Duration { get; set; } = string.Empty;
+    public List<Phase> Phases { get; set; } = [];
+}
+
+public class Phase
+{
+    public string Name { get; set; } = string.Empty;
+    public List<string> Tasks { get; set; } = [];
+}
--- src/FitnessExercises.Console/Program.cs
+++ src/FitnessExercises.Console/Program.cs
@@ -1,14 +1,11 @@
-﻿using System.Reflection.Metadata.Ecma335;
-using Azure;
+﻿using Azure;
 using Azure.AI.DocumentIntelligence;
+using FitnessExercises.Console;
 
 var endpoint = Environment.GetEnvironmentVariable("AZURE_DOCUMENT_INTELLIGENCE_ENDPOINT") ?? throw new InvalidOperationException("Environment variable AZURE_DOCUMENT_INTELLIGENCE_ENDPOINT is not set.");
 var key = Environment.GetEnvironmentVariable("AZURE_DOCUMENT_INTELLIGENCE_KEY") ?? throw new InvalidOperationException("Environment variable AZURE_DOCUMENT_INTELLIGENCE_KEY is not set.");
-var credential = new AzureKeyCredential(key);
 
-// ListAvailableModels(endpoint, credential);
-
-var client = new DocumentIntelligenceClient(new Uri(endpoint), credential);
+var client = new DocumentIntelligenceClient(new Uri(endpoint), new AzureKeyCredential(key));
 var modelId = "exercise-extractor-model";
 var fileUri = new Uri("https://raw.githubusercontent.com/simon-k/document-intelligence-example/refs/heads/main/data/dummy%20exercises%20-%20arm%20day%201.pdf");
 
@@ -17,51 +14,39 @@ Console.WriteLine($"Analyzing document {fileUri}...");
 var operation = await client.AnalyzeDocumentAsync(WaitUntil.Completed, modelId, fileUri);
 var analyzeResult = operation.Value;
 
-Console.WriteLine("----------------------------------------------------");
-Console.WriteLine($"Document was analyzed with model with ID: {analyzeResult.ModelId}");
-Console.WriteLine($"Number of documents found: {analyzeResult.Documents.Count}");
-Console.WriteLine($"Number of pages found: {analyzeResult.Pages.Count}");
-Console.WriteLine("----------------------------------------------------");
+var exercise = new Exercise();
 
 // Find fields in the analyzed documents
-foreach (AnalyzedDocument document in analyzeResult.Documents)
+foreach (var document in analyzeResult.Documents)
 {
-    document.Fields.TryGetValue("TrainingType", out var trainingTypeField);
-    document.Fields.TryGetValue("Duration", out var durationField);
+    document.Fields.TryGetValue("TrainingType", out var trainingTypeField);     //TODO: How to simplify this?
+    exercise.Type = trainingTypeField?.Content ?? string.Empty;
     
-    Console.WriteLine("Training Type: " + (trainingTypeField?.Content ?? "Not found"));
-    Console.WriteLine("Duration: " + (durationField?.Content ?? "Not found"));
+    document.Fields.TryGetValue("Duration", out var durationField);
+    exercise.Duration = durationField?.Content ?? string.Empty;
 }
 
-// Iterate over the document tables
+// Find tables in the analyzed document
 foreach (var table in analyzeResult.Tables)
 {
+    var phasesIndex = -1;
     foreach (var cell in table.Cells)
     {
-        if (string.IsNullOrWhiteSpace(cell.Content)) continue;
+        if (string.IsNullOrWhiteSpace(cell.Content)) continue; // Ignore empty cells
         
         switch (cell.ColumnIndex)
         {
             case 0:
-                Console.WriteLine($"Phase: {cell.Content}");
+                exercise.Phases.Add(new Phase { Name = cell.Content });
+                phasesIndex++;
                 break;
             case 1:
-                Console.WriteLine($"  Task: {cell.Content}");
+                if (phasesIndex < 0) continue; // No phase found yet
+                exercise.Phases[phasesIndex].Tasks.Add(cell.Content);
                 break;
         }
     }
 }
 
-return;
-
-void ListAvailableModels(string endpoint, AzureKeyCredential azureKeyCredential)
-{
-    Console.WriteLine("Awailable models");
-    var adminClient = new DocumentIntelligenceAdministrationClient(new Uri(endpoint), azureKeyCredential);
-    var models = adminClient.GetModels();
-    foreach (var model in models)
-    {
-        Console.WriteLine($"Model ID: {model.ModelId}, Model Description: {model.Description}, Created On: {model.CreatedOn}");
-    }
-}
-
+var exercisesJson = System.Text.Json.JsonSerializer.Serialize(exercise, new System.Text.Json.JsonSerializerOptions { WriteIndented = true });
+Console.WriteLine($"Result:{Environment.NewLine}{exercisesJson}");
